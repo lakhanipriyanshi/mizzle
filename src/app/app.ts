91da930ec6f8cb23fa6c776ea20d127f7db6e085
shruvaghani <shrutivaghani2004@gmail.com>
2026-02-19 11:03:07 +0530

    add : ng-icon

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { provideIcons } from '@ng-icons/core';
+import * as LucideIcons from '@ng-icons/lucide';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
+  providers: [provideIcons({ ...LucideIcons})],
 })
 export class App {
   protected readonly title = signal('mizzle');
