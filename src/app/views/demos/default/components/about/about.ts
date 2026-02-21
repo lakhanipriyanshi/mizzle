6bc670a8c2d939f16a964fa6a2c55f269588d0e7
lakhanipriyanshi <priyanshi.lakhani.it@gmail.com>
2026-02-21 10:20:24 +0530

    add : lucide angular icon

--- src/app/views/demos/default/components/about/about.ts
+++ src/app/views/demos/default/components/about/about.ts
@@ -1,9 +1,10 @@
 import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { LucideAngularModule } from 'lucide-angular';
 
 @Component({
   selector: 'default-about',
-  imports: [RouterLink],
+  imports: [RouterLink,LucideAngularModule],
   templateUrl: './about.html',
   styles: ``,
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
